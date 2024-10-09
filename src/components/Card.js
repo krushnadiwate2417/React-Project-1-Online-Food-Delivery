9cfd45a298b31d468bb6bbc594324dfe2c897fce
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2024-10-09 21:46:48 +0530

    Changes in css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,43 @@
 import { CDN_IMG_URL } from "../utils/constants";
-
+import { useState } from "react";
+import ViewMenu from "./ViewMenu";
 const Card = (props) => {
+  const [cardHover, setCardHover] = useState(false);
+
   const { resdata } = props;
 
   const { name, cloudinaryImageId, cuisines, avgRating, sla } = resdata?.info;
+
   return (
-    <div className="card">
+    <div
+      className="card"
+      onMouseEnter={() => {
+        setCardHover(true);
+      }}
+      onMouseLeave={() => {
+        setCardHover(false);
+      }}
+    >
       <img
         className="card-image"
         alt="any"
         src={CDN_IMG_URL + cloudinaryImageId}
       />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <ul className="card-list">
-        <li>{avgRating} stars</li>
-        <li>{sla.deliveryTime} minutes</li>
+      <h3 className="font-bold text-lg mt-4">{name}</h3>
+      <h5 className="mt-2 text-sm text-gray-600">{cuisines.join(", ")}</h5>
+      <ul className="card-list flex gap-2 justify-between text-center mt-2">
+        <li>
+          <h4 className="font-semibold ">Rating</h4>
+          {avgRating} stars
+        </li>
+        <li>
+          <h4 className="font-semibold">Delivery</h4>
+          {sla.deliveryTime} minutes
+        </li>
       </ul>
+      <div className=" text-center mt-3 font-bold">
+        {cardHover && <ViewMenu />}
+      </div>
     </div>
   );
 };
---
+++ src/components/ViewMenu.js
@@ -0,0 +1,5 @@
+const ViewMenu = () => {
+  return <h3 className="border-t border-solid border-black">View Menu</h3>;
+};
+
+export default ViewMenu;
